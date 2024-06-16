672a7c9e92ac0365cf8c843e3f6ec27ed322585d
Ray Luna <leon.luna.ray@gmail.com>
2024-06-15 21:41:51 -0700

    create recipe model

---
+++ Models/Recipe.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace AlDenteAPI.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Ingredients { get; set; } = string.Empty;
+        public string Instructions { get; set; } = string.Empty;
+    }
+}
