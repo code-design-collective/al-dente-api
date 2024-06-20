783f4394881aa0ca4cc443780b6fe6b3fb4a9408
Ray Luna <leon.luna.ray@gmail.com>
2024-06-19 22:56:00 -0700

    update cs proj file

--- Program.cs
+++ Program.cs
@@ -1,32 +1,29 @@
 using Microsoft.EntityFrameworkCore;
+using AlDenteAPI.Database;
+using AlDenteAPI.Endpoints;
+using AlDenteAPI.Extensions;
 
-using AlDenteAPI.Database
-;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// other services
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
+
+builder.Services.AddStackExchangeRedisCache(options =>
+    options.Configuration = builder.Configuration.GetConnectionString("Cache"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.ApplyMigrations();
 }
 
-// app.UseHttpsRedirection();
+app.MapRecipeEndpoints();
 
 app.MapGet("/test", () =>
 {
@@ -34,15 +31,5 @@ app.MapGet("/test", () =>
 })
 .WithName("GetTest")
 .WithOpenApi();
-
-// app.MapGet("/testdb", async () =>
-// {
-//     using var scope = app.Services.CreateScope();
-//     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-//     var recipeCount = await db.Recipes.CountAsync();
-//     return new { RecipeCount = recipeCount };
-// })
-// .WithName("GetTestDB")
-// .WithOpenApi();
 
 app.Run();
